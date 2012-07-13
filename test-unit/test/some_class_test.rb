36d41dd1c07f6f0c04c4ba452e36f23fb968a46a
Charlie Tanksley <charlie.tanksley@gmail.com>
2012-07-13 15:46:26 -0400

    update scripts so they actually work :)

--- test-unit/test/some_class_test.rb
+++ test-unit/test/some_class_test.rb
@@ -4,11 +4,11 @@ require 'test/unit'
 class SomeClassTest < Test::Unit::TestCase
   def test_first_method
     value = SomeClass.new('this')
-    assert 'siht', value.first_method
+    assert value.first_method, 'siht'
   end
 
   def test_second_method
-    value = SomeClass.new('this')
-    assert 'this', value.second_method
+    value = SomeClass.new('something')
+    assert value.second_method, 'something' 
   end
 end
